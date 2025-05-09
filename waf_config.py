f716a076f068be527eb419f08292b24ed8711971
pbuff07 <pbuff@qq.com>
2025-05-09 15:36:36 +0800

    新增奇安信网站卫士测试站

--- waf_config.py
+++ waf_config.py
@@ -21,6 +21,16 @@ WAF_CONFIGS = {
             "id": "id=([a-z0-9-]*)&"
         }
     },
+    "奇安信网站卫士": {  # 奇安信网站卫士
+        "sites": [
+            "https://mybase.qianxin.com/",
+        ],
+        "block_signatures": ["抱歉！您的访问可能对网站造成威胁，已被云防护拦截！"],
+        "test_payload": "/backup.mdb",
+        "match_info": {
+            "id": "eventID: ([a-z0-9-.]*)<\/p>"
+        }
+    },
     "阿里云": {  # 阿里云
         "sites": [
             "https://www.aliyun.com/",
@@ -28,28 +38,28 @@ WAF_CONFIGS = {
         "block_signatures": ["https://g.alicdn.com/sd/punish/waf_block.html"],
         "test_payload": "/backup.mdb",
     },
-    "玄武盾": {  # 阿里云
+    "玄武盾": {  # 玄武盾
         "sites": [
             "https://www.dbappsecurity.com.cn/",
         ],
         "block_signatures": ["https://error.websaas.cn/img/403s.png"],
         "test_payload": "/backup.mdb",
     },
-    "D盾": {  # 阿里云
+    "D盾": {  # D盾
         "sites": [
             "https://www.d99net.net/",
         ],
         "block_signatures": ["D盾_拦截提示"],
         "test_payload": "/?file=/etc/passwd",
     },
-    "宝塔云WAF": {  # 阿里云
+    "宝塔云WAF": {  # 宝塔云WAF
         "sites": [
             "https://www.bt.cn/",
         ],
         "block_signatures": ['"https://www.bt.cn/new/btwaf.html" target="_blank">堡塔云WAF'],
         "test_payload": "/backup.mdb",
     },
-    "安全狗": {  # 阿里云
+    "安全狗": {  # 安全狗
         "sites": [
             "https://www.safedog.cn/",
         ],
@@ -57,8 +67,6 @@ WAF_CONFIGS = {
         "test_payload": "/?file=/etc/passwd",
     },
 }
-
-# 添加更多WAF配置...
 
 def get_available_wafs():
     """获取所有可用的WAF名称列表"""
